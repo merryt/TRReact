f85bb15ab803dc7cca54928c04bbae3ebec5e713
Tyler Merry <tmerry@tmerry-mbp15r.local>
2015-10-20 08:15:52 -0600

    added chapter 4

--- chapter3/source/KanbanBoardContainer.js
+++ chapter3/source/KanbanBoardContainer.js
@@ -1,55 +1,177 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react/addons';
 import KanbanBoard from './KanbanBoard';
-import 'whatwg-fetch';
+// Polyfills
+import 'babel-core/polyfill'
 
-// If you're running the server locally, the URL will be, by default, localhost:3000
-// Also, the local server doesn't need an authorization header.
-const API_URL = 'http://kanbanapi.pro-react.com';
+const API_URL='http://localhost:3000'
 const API_HEADERS = {
   'Content-Type': 'application/json',
-  Authorization: 'any-string-you-like'// The Authorization is not needed for local server
+  Authorization: 'your-auth-id'
 }
 
 class KanbanBoardContainer extends Component {
   constructor(){
     super(...arguments);
+    window.update = React.addons.update
     this.state = {
-      cards: []
+      cards:[],
     };
   }
 
   componentDidMount(){
-    fetch(API_URL+'/cards', {headers: API_HEADERS})
+    fetch(`${API_URL}/cards`, {headers:API_HEADERS})
     .then((response) => response.json())
     .then((responseData) => {
-      this.setState({cards: responseData});
-    })
-    .catch((error) => {
-      console.log('Error fetching and parsing data', error);
+      this.setState({
+        cards: responseData
+      })
+
+      window.state = this.state;
     });
   }
-	addTask(cardId, taskName){
 
+  addTask(cardId, taskName){
+    // Keep a reference to the original state prior to the mutations
+    // in case we need to revert the optimistic changes in the UI
+    let prevState = this.state;
+
+    // Find the index of the card
+    let cardIndex = this.state.cards.findIndex((card)=>card.id == cardId);
+
+    // Create a new task with the given name and a temporary ID
+    let newTask = {id:Date.now(), name:taskName, done:false};
+    // Create a new object and push the new task to the array of tasks
+    let nextState = React.addons.update(this.state.cards, {
+                                          [cardIndex]: {
+                                            tasks: {$push: [newTask] }
+                                          }
+                                        });
+
+    // set the component state to the mutated object
+    this.setState({cards:nextState});
+
+    // Call the API to add the task on the server
+    fetch(`${API_URL}/cards/${cardId}/tasks`, {
+      method: 'post',
+      headers: API_HEADERS,
+      body: JSON.stringify(newTask)
+    })
+    .then((response) => {
+      if(response.ok){
+        return response.json()
+      } else {
+        // Throw an error if server response wasn't 'ok'
+        // so we can revert back the optimistic changes
+        // made to the UI.
+        throw new Error("Server response wasn't OK")
+      }
+    })
+    .then((responseData) => {
+      // When the server returns the definitive ID
+      // used for the new Task on the server, update it on React
+      newTask.id=responseData.id
+      this.setState({cards:nextState});
+    })
+    .catch((error) => {
+      this.setState(prevState);
+    });
   }
 
   deleteTask(cardId, taskId, taskIndex){
+    // Find the index of the card
+    let cardIndex = this.state.cards.findIndex((card)=>card.id == cardId);
 
+    // Keep a reference to the original state prior to the mutations
+    // in case we need to revert the optimistic changes in the UI
+    let prevState = this.state;
+
+    // Create a new object without the task
+    let nextState = React.addons.update(this.state.cards, {
+                                          [cardIndex]: {
+                                            tasks: {$splice: [[taskIndex,1]] }
+                                          }
+                                        });
+
+
+    // set the component state to the mutated object
+    this.setState({cards:nextState});
+
+    // Call the API to remove the task on the server
+    fetch(`${API_URL}/cards/${cardId}/tasks/${taskId}`, {
+      method: 'delete',
+      headers: API_HEADERS
+    })
+    .then((response) => {
+      if(!response.ok){
+        // Throw an error if server response wasn't 'ok'
+        // so we can revert back the optimistic changes
+        // made to the UI.
+        throw new Error("Server response wasn't OK")
+      }
+    })
+    .catch((error) => {
+      console.error("Fetch error:",error)
+      this.setState(prevState);
+    });
   }
 
   toggleTask(cardId, taskId, taskIndex){
+    // Keep a reference to the original state prior to the mutations
+    // in case we need to revert the optimistic changes in the UI
+    let prevState = this.state;
 
+    // Find the index of the card
+    let cardIndex = this.state.cards.findIndex((card)=>card.id == cardId);
+    // Save a reference to the task's 'done' value
+    let newDoneValue;
+    // Using the $apply command, we will change the done value to its opposite,
+    let nextState = React.addons.update(
+                        this.state.cards, {
+                          [cardIndex]: {
+                            tasks: {
+                              [taskIndex]: {
+                                done: { $apply: (done) => {
+                                    newDoneValue = !done
+                                    return newDoneValue;
+                                  }
+                                }
+                              }
+                            }
+                          }
+                        });
+
+    // set the component state to the mutated object
+    this.setState({cards:nextState});
+
+
+    // Call the API to toggle the task on the server
+    fetch(`${API_URL}/cards/${cardId}/tasks/${taskId}`, {
+    	method: 'put',
+    	headers: API_HEADERS,
+    	body: JSON.stringify({done:newDoneValue})
+    })
+    .then((response) => {
+      if(!response.ok){
+        // Throw an error if server response wasn't 'ok'
+        // so we can revert back the optimistic changes
+        // made to the UI.
+        throw new Error("Server response wasn't OK")
+      }
+    })
+    .catch((error) => {
+      console.error("Fetch error:",error)
+      this.setState(prevState);
+    });
   }
 
   render() {
     return (
-      <KanbanBoard cards={this.state.cards}
-                   taskCallbacks={{
-                          toggle: this.toggleTask.bind(this),
-                          delete: this.deleteTask.bind(this),
-                             add: this.addTask.bind(this) }} />
+      <KanbanBoard cards={this.state.cards} taskCallbacks={{
+           toggle: this.toggleTask.bind(this),
+           delete: this.deleteTask.bind(this),
+           add: this.addTask.bind(this) }} />
     )
   }
-
 }
 
 export default KanbanBoardContainer;
